7b9c9863f8aa32fba7908de20ba0fde1e2627817
Bankole Ahmed <kidrolex19@gmail.com>
2020-05-14 00:54:58 +0100

    :zap: Configured Handlebars

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const handlebars = require("express-handlebars")
+const hbs = require("express-handlebars")
 const config = require('./config/configuration')
 const passport = require('passport')
 const mongoose = require('mongoose')
@@ -12,7 +12,7 @@ const connectionString = `mongodb://${host}:${port}/${name}`
 const app = express()
 
 //Configure MongoDB Database
-mongoose.connect(connectionString, { useNewUrlParser: true })
+mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology:true })
     .then(response => {
       console.log('MongoDB Database IS Running Successfully')
     })
@@ -34,6 +34,10 @@ app.use(passport.session())
 //Configure bodyParser
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
+
+//Configuring Handlebars For Views
+app.set('handlebars', hbs({defaultLayout: 'main' }));
+app.engine('view engine', handlebars)
 
 app.get('/', (req, res)=>{
 	res.send('Hello from the server side')
