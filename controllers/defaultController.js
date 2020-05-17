226872754bf1500970d01ee2f41bdd84fa382df8
Bankole Ahmed <kidrolex19@gmail.com>
2020-05-18 00:53:12 +0100

    :rocket:  Added updateProduct controller

--- controllers/defaultController.js
+++ controllers/defaultController.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 
 module.exports = {
 
+//Display List Of Product Controller
   getProducts: (req, res) =>{
     Product.find((err , products)=> {
       if(!err){
@@ -15,6 +16,7 @@ module.exports = {
     })
   },
 
+//Add Product Controller
   createProduct: (req, res)=>{
     const newProduct = new Product({
       name: req.body.name,
@@ -45,5 +47,40 @@ module.exports = {
         res.send('error: ' + err)
       })
   )
-  }
+},
+
+//Update Product Controller
+updateProduct: (req, res)=> {
+  const id = req.params.id
+  Product.findById(id)
+    .then(product => {
+      product.name = req.body.name,
+      product.tag = req.body.tag,
+      product.price = req.body.price,
+      product.seller = req.body.seller,
+      product.location= req.body.location,
+      product.description= req.body.description
+    },
+    Product.findOne({
+      name: req.body.name
+    })
+    .then(product => {
+      if (!product) {
+          Product.create()
+            .then(product => {
+              res.json({ status: product.name + ' has been updated' })
+            })
+            .catch(err => {
+              res.send('error: ' + err)
+            })
+        }
+      else {
+        res.json({ error: 'Product already exists' })
+      }
+    })
+    .catch(err => {
+      res.send('error: ' + err)
+    })
+)
+}
 }
--- routes/defaultRoutes.js
+++ routes/defaultRoutes.js
@@ -7,7 +7,9 @@ router.use(cors())
 router.route('/products')
   .get(defaultController.getProducts)
 
-router.route('/products/create')
+router.route('/product/create')
   .post(defaultController.createProduct)
 
+router.route('/product/edit/:id')
+  .put(defaultController.updateProduct)
 module.exports = router
